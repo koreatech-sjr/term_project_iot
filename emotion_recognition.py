fdc83d044c4ecbecdc4a2a0a30504427d81fa176
sonjaerock <kadamon2007@gmail.com>
2018-05-18 11:12:14 +0900

    remove ipython

--- emotion_recognition.py
+++ emotion_recognition.py
@@ -6,7 +6,6 @@ assert subscription_key
 face_api_url = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect"
 
 import requests
-from IPython.display import HTML
 
 headers = {'Ocp-Apim-Subscription-Key': subscription_key }
 
@@ -20,6 +19,5 @@ image_data = {'url': './image.jpeg'}
 
 response = requests.post(face_api_url, params=params, headers=headers, json={"url": 'https://ggia.berkeley.edu/assets/general/GGIA-HumanFace.jpg'})
 faces = response.json()
-HTML("<font size='5'>Detected <font color='blue'>%d</font> faces in the image</font>"%len(faces))
 
 print(faces)
