6f303c0db57f10a2b316b29f82e259f1d54430ef
sonjaerock <kadamon2007@gmail.com>
2018-05-29 12:25:24 +0900

    except add

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from bluetooth import *
 # Create the client socket
 client_socket = BluetoothSocket(RFCOMM)
 
-client_socket.connect(("00:12:D2:5A:BD:E4", 3))
+client_socket.connect(("b8:27:eb:6b:4e:9a", 3))
 
 client_socket.send("Hello World")
 
