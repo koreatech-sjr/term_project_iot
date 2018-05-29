c6866e59614c2b62dd0efbcffce642af00b76ce0
sonjaerock <kadamon2007@gmail.com>
2018-05-29 11:37:20 +0900

    test

--- local_path_test.py
+++ local_path_test.py
@@ -29,7 +29,7 @@ takePictureCommand = "raspistill -br 50 -t 1000 -o " + imagepath
 
 while True:
     os.system(takePictureCommand)  
-
+    print("take a picture!")
     img = open(expanduser('./face.jpg'), 'rb')
     response = requests.post(url, data=img, headers=headers)
     pprint(response.json())
