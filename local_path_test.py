85acde7363400f60a9a4d816fe9948be5e9e2533
sonjaerock <kadamon2007@gmail.com>
2018-05-29 11:39:50 +0900

    except add

--- local_path_test.py
+++ local_path_test.py
@@ -26,17 +26,20 @@ url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?%s' % 
 imagepath = "./face.jpg"
 takePictureCommand = "raspistill -br 50 -t 1000 -o " + imagepath 
 
+try:
+    while True:
+        os.system(takePictureCommand)  
+        print("take a picture!")
+        img = open(expanduser('./face.jpg'), 'rb')
+        response = requests.post(url, data=img, headers=headers)
+        pprint(response.json())
+        if response.status_code != 200:
+            raise ValueError(
+                'Request to Azure returned an error %s, the response is:\n%s'
+                % (response.status_code, response.text)
+            )
 
-while True:
-    os.system(takePictureCommand)  
-    print("take a picture!")
-    img = open(expanduser('./face.jpg'), 'rb')
-    response = requests.post(url, data=img, headers=headers)
-    pprint(response.json())
-    if response.status_code != 200:
-        raise ValueError(
-            'Request to Azure returned an error %s, the response is:\n%s'
-            % (response.status_code, response.text)
-        )
+        os.remove(imagepath)
+except KeyboardInterrupt:
+    print("Finished!")
 
-    os.remove(imagepath)
